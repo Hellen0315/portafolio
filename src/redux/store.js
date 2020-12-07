3af63ac6b72a375dc652ab97b7b89ac799045c1c
Hellen0315 <maryavril315@gmail.com>
2020-12-07 13:14:29 -0500

    delete

--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import results from "./reducers/results";
-
-const store = createStore(results, applyMiddleware(thunk));
-
-export default store;
